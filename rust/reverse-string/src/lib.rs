85aef57fe135e0b31a747c464bacfe6c79458907
Tanguy <pauwelstanguy@gmail.com>
2024-11-15 19:11:39 +0100

    add comment to reverse string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn reverse(input: &str) -> String  {
-     input.chars().rev().collect::<String>()
+     input
+         .chars() // Extract caractère from the str into an iterator
+         .rev() // Reverse chars
+         .collect::<String>() // Reassemble iterator's element into a String  
 }
